fe63def997a68cc611161ab859b1bcf7e8ab7542
imgulali <gulali.official@gmail.com>
2024-04-25 00:14:32 +0500

    Added: Server Code

---
+++ server/config/Constants.js
@@ -0,0 +1,3 @@
+export const PORT = 5000;
+export const MongoURI = "mongodb://localhost:27017/bitts";
+export const JwtKey = "x4rNYRV3984pkP#pZbwg0lV&**v8xAuU%gYXyLhN79k7@oCff209J2y3!I";
---
+++ server/validators/UserValidator.js
@@ -0,0 +1,47 @@
+import joi from "joi";
+
+export const RegisterUserSchema = joi.object({
+  name: joi.string().required().messages({
+    "any.required": "Name is required",
+    "string.empty": "Name is required",
+  }),
+
+  email: joi.string().email().required().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+
+  password: joi.string().required().min(6).messages({
+    "any.required": "Password is required",
+    "string.min": "Password should be atleast of 6 chars",
+  }),
+});
+
+export const LoginUserSchema = joi.object({
+  email: joi.string().email().required().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+
+  password: joi.string().required().min(6).messages({
+    "any.required": "Password is required",
+    "string.min": "Password should be atleast of 6 chars",
+  }),
+});
+
+export const UpdateUserSchema = joi.object({
+  name: joi.string().messages({
+    "any.required": "Name is required",
+    "string.empty": "Name is required",
+  }),
+
+  email: joi.string().email().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+
+  password: joi.string().min(6).messages({
+    "any.required": "Password is required",
+    "string.min": "Password should be atleast of 6 chars",
+  }),
+});
