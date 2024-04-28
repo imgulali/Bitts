01edcf3fcf91d5807c254079dcae6d6a58c0aa47
imgulali <gulali.official@gmail.com>
2024-04-28 21:00:57 +0500

    Added: Context

--- web/src/components/ProtectedRoutes.jsx
+++ web/src/components/ProtectedRoutes.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 const ProtectedRoutes = () => {
-  const authToken = null;
-  return authToken ? <Outlet /> : <Navigate to={"/login"} />;
+  const { currentUser } = useAuth();
+  return currentUser ? <Outlet /> : <Navigate to={"/login"} />;
 };
 
 export default ProtectedRoutes;
---
+++ web/src/contexts/AuthContext.jsx
@@ -0,0 +1,95 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  fetchUserApi,
+  loginUserApi,
+  registerUserApi,
+  updateUserApi,
+} from "../api/authAPI";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const registerUser = async (formData) => {
+    const { error, data } = await registerUserApi(formData);
+    if (data) {
+      const user = data.data;
+      const { authToken } = user;
+      localStorage.setItem("authToken", JSON.stringify(authToken));
+      setCurrentUser(user);
+    }
+    setLoading(false);
+    return error;
+  };
+
+  const loginUser = async (formData) => {
+    const { error, data } = await loginUserApi(formData);
+    if (data) {
+      const user = data.data;
+      const { authToken } = user;
+      localStorage.setItem("authToken", JSON.stringify(authToken));
+      setCurrentUser(user);
+    }
+    setLoading(false);
+    return error;
+  };
+
+  const fetchUser = async () => {
+    const authToken = localStorage.getItem("authToken");
+    if (authToken) {
+      const parsed = await JSON.parse(authToken);
+      const { error, data } = await fetchUserApi(parsed);
+      if (data) {
+        const user = data.data;
+        setCurrentUser(user);
+      }
+      if (error) {
+        console.error(error);
+      }
+    }
+    setLoading(false);
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem("authToken");
+    setCurrentUser(null);
+  };
+
+  const updateUser = async (formData) => {
+    const getAuthToken = localStorage.getItem("authToken");
+    if (getAuthToken) {
+      const authToken = await JSON.parse(getAuthToken);
+      const { error, data } = await updateUserApi(authToken, formData);
+      if (error) {
+        return error;
+      }
+      const user = data.data;
+      setCurrentUser({ ...currentUser, ...user });
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const value = {
+    currentUser,
+    registerUser,
+    loginUser,
+    logoutUser,
+    updateUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
