587ff4644c9b2696a2a375296792c2a52673f2d8
imgulali <gulali.official@gmail.com>
2024-04-29 13:57:26 +0500

    Added: Dotenv Configuration

--- server/config/Constants.js
+++ server/config/Constants.js
@@ -1,3 +1,6 @@
-export const PORT = 5000;
-export const MongoURI = "mongodb://localhost:27017/bitts";
-export const JwtKey = "x4rNYRV3984pkP#pZbwg0lV&**v8xAuU%gYXyLhN79k7@oCff209J2y3!I";
+import { configDotenv } from "dotenv"
+configDotenv();
+
+export const PORT = process.env.PORT || 5000;
+export const MongoURI = process.env.MongoURI; //Add a Mongodb Url
+export const JwtKey = process.env.JwtKey; // Add your Jwt Key
